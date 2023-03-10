09af7b8961d043a7f323cca068171ef8759f7f51
charles busso <109078429+charlesbusso@users.noreply.github.com>
2023-03-10 09:55:21 -0300

    ok

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+//https://flagsapi.com/BR/flat/64.png
 
 let apiKey = "f7beb48e5ddcfaa49760ec0218e9dbb8";
-let apiCountryURL = "https://flagsapi.com/BR/flat/64.png";
+let apiCountryURL = "https://countryflagsapi.com/png/";
 let cityInput = document.querySelector("#city-input");
 let searchBtn = document.getElementById("search");
 let cityElement = document.querySelector("#city");
@@ -10,28 +11,35 @@ let weatherIconElement = document.querySelector("#weather-icon");
 let countryElement = document.querySelector("#country");
 let umidityElement = document.querySelector("#umidity span");
 let windElement = document.querySelector("#wind span");
+
+
 //funçoes
 
-const getWeatherData = async(city) => {
-    const apiWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid={apiKey}&lang=pt_br";
+const getWeatherData = async (city) => {
+    const apiWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid={apiKey}&lang=pt_br';
+
     const res = await fetch(apiWeatherURL);
     const data = await res.json();
+
     console.log(data);
 };
 
 const showWeatherData = (city) => {
     getWeatherData(city);
+
+ 
+   
+   
 };
-
-searchBtn.addEventListener("click", (e) => {
-
+searchBtn.addEventListener('click',(e) => {
     e.preventDefault();
-
+   
     const city = cityInput.value;
 
     showWeatherData(city);
-    
+
 });
+
 
 
 
